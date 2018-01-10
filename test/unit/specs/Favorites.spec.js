5870fad0d91782eb463b45cc4637cc10222d3f93
Mustafa Al Ammar <malammar@gmail.com>
2018-01-10 13:55:02 -0800

    Fixed indentation in favorites spec

--- test/unit/specs/Favorites.spec.js
+++ test/unit/specs/Favorites.spec.js
@@ -2,7 +2,7 @@ import Favorites from '@/utils/favorites'
 
 describe('Favorites.js', () => {
   it('should have an empty favorites list', () => {
-  	let favorites = new Favorites()
+    let favorites = new Favorites()
     expect(favorites.get())
     .toEqual({})
   })
